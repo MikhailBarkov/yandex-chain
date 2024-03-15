bc97cfb3a35a79e73cade1bf089713256a7c54ab
mikhail.barkov <mikhail.barkov@uno-soft.ru>
2024-03-15 16:57:17 +0400

    Add async mode

--- yandex_chain/YandexGPT.py
+++ yandex_chain/YandexGPT.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 from typing import Any, List, Mapping, Optional
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 import requests
@@ -8,11 +9,14 @@ from yandex_chain.util import YAuth, YException
 from tenacity import Retrying, RetryError, stop_after_attempt, wait_fixed
 
 class YandexLLM(LLM):
+    async_retries: int = 5
+    async_sleep_interval: float = 0.1
     temperature : float = 1.0
     max_tokens : int = 1500
     sleep_interval : float = 1.0
     retries = 3
     use_lite = True
+    use_async = False
     instruction_text : str = None
     instruction_id : str = None
     iam_token : str = None
@@ -92,19 +96,48 @@ class YandexLLM(LLM):
         try:
             for attempt in Retrying(stop=stop_after_attempt(self.retries),wait=wait_fixed(self.sleep_interval)):
                 with attempt:
-                    res = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
-                            headers=headers, json=req)
-                    js = res.json()
-                    if 'result' in js:
-                        res = js['result']
-                        usage = res['usage']
-                        self.totalTokens += int(usage['totalTokens'])
-                        self.completionTokens += int(usage['completionTokens'])
-                        self.inputTextTokens += int(usage['inputTextTokens'])
-                        return res['alternatives'][0]['message'] if return_message else res['alternatives'][0]['message']['text']
-                    raise YException(f"Cannot process YandexGPT request, result received: {js}")
+                    if self.use_async is False:
+                        return self._send_sync(headers, req, return_message)
+                    else:
+                        return self._send_async(headers, req, return_message)
         except RetryError:
-            raise YException(f"Error calling YandexGPT after {self.retries} retries. Result returned:\n{js}")
+            raise YException(f"Error calling YandexGPT after {self.retries} retries.")
+
+    def _send_sync(self, headers, req, return_message):
+        res = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
+            headers=headers, json=req)
+        js = res.json()
+        if 'result' in js:
+            res = js['result']
+            return self._parse_result(res, return_message)
+        raise YException(f"Cannot process YandexGPT request, result received: {js}")
+
+    def _send_async(self, headers, req, return_message):
+        res = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync",
+            headers=headers, json=req)
+        js = res.json()
+        if 'id' in js:
+            operation_id = js['id']
+        else:
+            raise YException(f"Cannot process YandexGPT async request, result received: {js}")
+        for _ in range(self.async_retries):
+            sleep(self.async_sleep_interval)
+            res = requests.get(f"https://llm.api.cloud.yandex.net/operations/{operation_id}",
+                headers=headers, json=req)
+            js = res.json()
+            if js['done'] is True:
+                response = js.get('response')
+                if response is None:
+                    raise YException(f"An error occurred while generating the response. Result returned: {js}")
+                else:
+                    return self._parse_result(response, return_message)
+
+    def _parse_result(self, res, return_message):
+        usage = res['usage']
+        self.totalTokens += int(usage['totalTokens'])
+        self.completionTokens += int(usage['completionTokens'])
+        self.inputTextTokens += int(usage['inputTextTokens'])
+        return res['alternatives'][0]['message'] if return_message else res['alternatives'][0]['message']['text']
 
     def resetUsage(self):
         self.totalTokens = 0
